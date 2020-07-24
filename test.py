5dda079044e804d04f253c66c939408e181f5cbe
Adrien Oyono <Adrien.Owono@ecmwf.int>
2020-07-24 15:25:59 +0100

    applies migration and formatting to tests

--- test.py
+++ test.py
@@ -32,8 +32,7 @@ from C3Schecker import utils
 from C3Schecker.utils import functions as fct
 
 
-class Checker_test():
-
+class Checker_test:
     def __init__(self, keep):
 
         self.failed = 0
@@ -50,15 +49,13 @@ class Checker_test():
 
         listnc = []
         listcdl = fct.get_immediate_fullpathfiles(
-            os.path.join(
-                os.path.dirname(
-                    os.path.realpath(__file__)) +
-                cdldir),
+            os.path.join(os.path.dirname(os.path.realpath(__file__)) + cdldir),
             [],
-            "cdl")
+            "cdl",
+        )
 
         tmpdir = tempfile.mkdtemp(dir="/tmp")
-        print ""
+        print("")
 
         for f in listcdl:
             ncfile = tmpdir + os.sep + f.split(os.sep)[-1].replace("cdl", "nc")
@@ -67,10 +64,8 @@ class Checker_test():
                 command = "ncgen -k 4 -o " + ncfile + " " + f
                 os.popen(command)
                 fct.prBlack(
-                    "Creation of temporary NetCDF file: " +
-                    ncfile +
-                    " from " +
-                    f)
+                    "Creation of temporary NetCDF file: " + ncfile + " from " + f
+                )
                 listnc.append(ncfile)
             except:
                 continue
@@ -80,58 +75,57 @@ class Checker_test():
     def test_compliantfiles(self):
 
         cdldirmain = "/tests/data/compliant/"
-        listsubdir = fct.get_immediate_subdirectories(os.path.join(
-            os.path.dirname(os.path.realpath(__file__))) + cdldirmain)
+        listsubdir = fct.get_immediate_subdirectories(
+            os.path.join(os.path.dirname(os.path.realpath(__file__))) + cdldirmain
+        )
 
         for subdir in listsubdir:
 
             listnc, tmpdir = self.create_netcdffile(cdldirmain + subdir)
-            print ""
+            print("")
 
             for ncfile in listnc:
 
-                status = C3S_checker.check(['-t', subdir, ncfile])
+                status = C3S_checker.check(["-t", subdir, ncfile])
                 fct.prBlack(
-                    "Check " +
-                    ncfile +
-                    " with configuration [" +
-                    subdir.upper() +
-                    "]")
+                    "Check " + ncfile + " with configuration [" + subdir.upper() + "]"
+                )
                 try:
                     assert status == 1
-                    print subdir + ncfile
+                    print((subdir + ncfile))
                     fct.prGreen(" PASSED - Checker status is 1 (Successful)")
                 except:
                     self.failed += 1
-                    fct.prRed(
-                        " FAILED - Checker status should be 1 (Successful)")
+                    fct.prRed(" FAILED - Checker status should be 1 (Successful)")
 
             if not (self.keep):
                 shutil.rmtree(tmpdir)
-                print "Temporary files deleted"
+                print("Temporary files deleted")
 
     def test_noncompliantfiles(self):
 
         cdldirmain = "/tests/data/non_compliant/"
-        listsubdir = fct.get_immediate_subdirectories(os.path.join(
-            os.path.dirname(os.path.realpath(__file__))) + cdldirmain)
+        listsubdir = fct.get_immediate_subdirectories(
+            os.path.join(os.path.dirname(os.path.realpath(__file__))) + cdldirmain
+        )
 
         for subdir in listsubdir:
 
             listnc, tmpdir = self.create_netcdffile(cdldirmain + subdir)
-            print
+            print()
             for ncfile in listnc:
 
-                status = C3S_checker.check(['-t', subdir, ncfile])
-                print "Check " + ncfile + " with configuration [" + subdir.upper() + "]"
+                status = C3S_checker.check(["-t", subdir, ncfile])
+                print(
+                    ("Check " + ncfile + " with configuration [" + subdir.upper() + "]")
+                )
                 try:
                     assert status == 0
                     fct.prGreen(" PASSED - Checker status is 1 (Unsuccessful)")
 
                 except:
                     self.failed += 1
-                    fct.prRed(
-                        " FAILED - Checker status should be 0 (Unsuccessful)")
+                    fct.prRed(" FAILED - Checker status should be 0 (Unsuccessful)")
 
             shutil.rmtree(tmpdir)
 
@@ -140,16 +134,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="", epilog=" ")
 
-    parser.add_argument('-k', '--keep',
-                        action="store_true",
-                        help="Do not remove temporary files")
+    parser.add_argument(
+        "-k", "--keep", action="store_true", help="Do not remove temporary files"
+    )
     args = parser.parse_args(sys.argv[1:])
 
     a = datetime.datetime.now()
     fct.prGreen("========== Test session start - " + str(a) + "=========")
 
-    fct.prCyan("Plateform: " + str(platform.system()) +
-               " - " + str(platform.version()))
+    fct.prCyan("Plateform: " + str(platform.system()) + " - " + str(platform.version()))
     fct.prCyan("Python: " + str(platform.python_version()))
 
     failed = Checker_test(args.keep).get_failed()
@@ -160,10 +153,20 @@ if __name__ == "__main__":
 
     if failed > 0:
         fct.prRedBold("Something Wrong")
-        fct.prRed("========== Test session end - Elapsed time : " +
-                  str(e[0]) + " minutes " + str(e[1]) + "seconds ===========")
+        fct.prRed(
+            "========== Test session end - Elapsed time : "
+            + str(e[0])
+            + " minutes "
+            + str(e[1])
+            + "seconds ==========="
+        )
 
     else:
         fct.prGreenBold("Tests OK")
-        fct.prGreen("========== Test session end - Elapsed time : " +
-                    str(e[0]) + " minutes " + str(e[1]) + "seconds ===========")
+        fct.prGreen(
+            "========== Test session end - Elapsed time : "
+            + str(e[0])
+            + " minutes "
+            + str(e[1])
+            + "seconds ==========="
+        )
